4093ae908343983eabfc4ed2e08deb1859c0f005
Cristian Vega <cristianvega73212828@gmail.com>
2026-07-16 00:26:42 -0500

    Redis Service and cache in routes

--- backend/server/routes.go
+++ backend/server/routes.go
@@ -1,9 +1,14 @@
 package server
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"queue-go/models"
+	"queue-go/service"
 	"queue-go/utils"
+	"time"
 )
 
 func Routes() {
@@ -13,16 +18,36 @@ func Routes() {
 
 	Http.Get("/sp500/columns", func(w http.ResponseWriter, r *http.Request) {
 
-		model := models.Model{}
-		columns, err := model.Columns(models.SP500{})
+		var response any
+		cacheKey := "columns:sp500"
+		rsv := service.RedisSrvc{}
+		ctx := context.Background()
 
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
+		//TODO Test if it works
+
+		if rsv.Has(ctx, cacheKey) {
+			response = rsv.Get(ctx, cacheKey, "nothing")
+
+		} else {
+			model := models.Model{}
+			columns, err := model.Columns(models.SP500{})
+
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+			}
+
+			response = map[string]any{
+				"success": true,
+				"data":    columns,
+			}
+
+			data, _ := json.Marshal(response)
+
+			err = rsv.Set(ctx, cacheKey, data, time.Hour)
+
+			fmt.Println(err.Error())
 		}
 
-		utils.ResponseJson(map[string]any{
-			"success": true,
-			"data":    columns,
-		}, w, r)
+		utils.ResponseJson(response, w, r)
 	})
 }
--- backend/service/redis.go
+++ backend/service/redis.go
@@ -1,6 +1,13 @@
 package service
 
-import "github.com/redis/go-redis/v9"
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
 
 type (
 	RedisSrvc struct{}
@@ -14,4 +21,91 @@ func (r *RedisSrvc) Connect() *redis.Client {
 	})
 
 	return rdb
+}
+
+func (r *RedisSrvc) Get(ctx context.Context, key string, valueDefault any) any {
+	rdb := r.Connect()
+	defer rdb.Close()
+
+	if !r.Has(ctx, key) {
+		return valueDefault
+	}
+
+	t, err := rdb.Type(ctx, key).Result()
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	var result any
+	switch t {
+	case "string":
+		result_rdb, _ := rdb.Get(ctx, key).Result()
+		var unmarshaledResult any
+		err := json.Unmarshal([]byte(result_rdb), &unmarshaledResult)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		result = unmarshaledResult
+	case "hash":
+		result, _ = rdb.HGetAll(ctx, key).Result()
+	case "list":
+
+	}
+
+	return result
+}
+
+func (r *RedisSrvc) Has(ctx context.Context, key string) bool {
+	rdb := r.Connect()
+	defer rdb.Close()
+
+	exist, err := rdb.Exists(ctx, key).Result()
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return false
+	}
+
+	if exist == 1 {
+		return true
+	} else {
+		return false
+	}
+}
+
+func (r *RedisSrvc) HSet(ctx context.Context, key string, values any, ttl time.Duration) error {
+	rdb := r.Connect()
+	defer rdb.Close()
+
+	err := rdb.HSet(ctx, key, values).Err()
+
+	if err != nil {
+		return err
+	}
+
+	if ttl != 0 {
+		err := rdb.Expire(ctx, key, ttl).Err()
+
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+
+}
+
+func (r *RedisSrvc) Set(ctx context.Context, key string, values any, ttl time.Duration) error {
+	rdb := r.Connect()
+	defer rdb.Close()
+
+	err := rdb.Set(ctx, key, values, ttl).Err()
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+
 }
